9fb8de7d9e365856389c2208ec7a9ba85a67ff05
alansai <61389125+alansai@users.noreply.github.com>
2026-01-18 18:49:40 -0500

    Add files via upload
    player class!!

---
+++ Player.py
@@ -0,0 +1,75 @@
+import random
+
+
+# Author alansai
+'''
+    Represent a Player for Mod/Sim Fall 2026 Final Project (MBHS)
+    A Player has an id (like "P1"), as well as preferences for 2 strategies (which sum to 1.0).
+    and keeps track of how many numGamesPlayed they've played, plus their tot score and avg score.
+
+    Inspiration drawn from the work done by Dr. Andrew Davison (PSU)
+
+    sample usage: p1 = Player("P1")
+'''
+
+
+class Player:
+    def __init__(self, id):
+        self.id = id
+        # Create preferences as (0.5, 0.5 at beginning)
+        self.prefs = [0.5, 0.5]
+        self.currentGameScore = 0
+        # number of games, total score, and average score which is totalScore/numGamesPlayed
+        self.numGamesPlayed = 0
+        self.totalScore = 0.0
+        self.averageScore = 0.0
+        # history of preferences as list to be plotted
+        self.history = []
+        self.history2 = []
+
+    def getId(self):
+        return self.id
+
+    def getPrefs(self):
+        return self.prefs
+
+    def getStrategy(self):
+        # randomly selects strategy through preference
+        if random.random() < self.prefs[0]:
+            return 0
+        else:
+            return 1
+
+    def updateScore(self, score):
+        # updates score after preference
+        self.totalScore += score
+        self.numGamesPlayed += 1
+
+    def updatePrefs(self, strategy, score):
+        # utilizes formula to get new preference, makes sure it is within boundaries
+        if (self.prefs[0] != 1.0 or self.prefs[1] != 1.0):
+            if strategy == 0:
+                self.prefs[0] = self.prefs[0] + (score - self.averageScore) / 100.0
+                if self.prefs[0] > 1.0:
+                    self.prefs[0] = 1.0
+                elif self.prefs[0] < 0.0:
+                    self.prefs[0] = 0.0
+                self.prefs[1] = 1.0 - self.prefs[0]
+            else:
+                self.prefs[0] = self.prefs[0] - (score - self.averageScore) / 100.0
+                if self.prefs[0] > 1.0:
+                    self.prefs[0] = 1.0
+                elif self.prefs[0] < 0.0:
+                    self.prefs[0] = 0.0
+                self.prefs[1] = 1.0 - self.prefs[0]
+        print(self.prefs[0])
+        # updates average
+        self.averageScore = self.totalScore / self.numGamesPlayed
+        self.history.append(self.prefs[0])
+        self.history2.append(self.prefs[1])
+
+    # for final preferences
+    def __str__(self):
+        a = self.prefs[0]
+        b = 1.0 - a
+        return f"{self.id}: <{a:.2f}, {b:.2f}>"
